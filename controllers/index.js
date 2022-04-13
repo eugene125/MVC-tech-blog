27a224870c2a5535236bab1d9e810c6aad11aadb
Eugene Ahn <eugeneahn125@gmail.com>
2022-04-13 18:49:01 -0500

    worked on handlebars and routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,10 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
+const dashRoutes = require('./dashboard-routes');
 
-const apiRoutes = require("./api");
-const dashboardRoutes = require("./dashboard-routes");
-const homeRoutes = require("./home-routes");
-
-router.use("/api", apiRoutes);
-router.use("/dashboard", dashboardRoutes);
-router.use("/", homeRoutes);
+router.use('/api', apiRoutes),
+router.use('/', homeRoutes);
+router.use('/dashboard', dashRoutes);
 
 module.exports = router;
