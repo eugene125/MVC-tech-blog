1503cb62d227e95cad9f4c468edbded0daeb2533
Eugene Ahn <eugeneahn125@gmail.com>
2022-04-13 20:51:47 -0500

    updated user-seed file

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -2,38 +2,32 @@ const { User } = require("../models");
 
 const userData =[
     {
-        first_name: "Brian",
-        last_name: "Griffin",
-        email: "BryanG@gmail.com",
+        username: "Brian_Griffin",
+        email: "BrianG@gmail.com",
         password: "Password1"
     },
     {
-        first_name: "Stewie",
-        last_name: "Griffin",
+        username: "Stewie_Griffin",
         email: "StewieG@gmail.com",
         password: "Password2"
     },
     {
-        first_name: "Lois",
-        last_name: "Griffin",
+        username: "Lois_Griffin",
         email: "LoisG@gmail.com",
         password: "Password3"
     },
     {
-        first_name: "Peter",
-        last_name: "Griffin",
+        username: "Peter_Griffin",
         email: "PeterG@gmail.com",
         password: "Password4"
     },
     {
-        first_name: "Meg",
-        last_name: "Griffin",
+        username: "Meg_Griffin",
         email: "MegG@gmail.com",
         password: "Password5"
     },
     {
-        first_name: "Chris",
-        last_name: "Griffin",
+        username: "Chris_Griffin",
         email: "ChrisG@gmail.com",
         password: "Password6"
     }
